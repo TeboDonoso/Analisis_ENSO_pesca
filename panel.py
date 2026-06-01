4ae9b4372027d239400541e1bf802bec7ab807cc
Esteban <69662062+TeboDonoso@users.noreply.github.com>
2026-05-31 22:26:45 -0400

    Update panel.py

--- panel.py
+++ panel.py
@@ -149,10 +149,11 @@ else:
 
 # --- NUEVO: SECCIÓN DE REFERENCIAS Y METODOLOGÍA ---
 st.markdown("---")
-st.markdown("### 📚 Fuentes de Datos y Replicabilidad")
+st.markdown("### 📚 Fuentes de Datos")
 st.markdown("""
 * **Datos Pesqueros:** Histórico de Desembarques Totales (2000-2024), extraídos del *Anuario Estadístico* del **Servicio Nacional de Pesca y Acuicultura (SERNAPESCA)** de Chile (http://anuario.sernapesca.dataobservatory.net/).
 * **Datos Oceanográficos:** Clasificación del fenómeno ENOS (El Niño-Oscilación del Sur) basada en el **Índice del Niño Oceánico (ONI)**, calculado por el *Climate Prediction Center* de la **NOAA** (Región Niño 3.4).
 * **Metodología de Intensidad:** La intensidad fue categorizada de -3 a +3 según las anomalías térmicas mensuales para modelar correlaciones lineales.
 * **Desarrollo:** Panel analítico construido utilizando Python, Pandas, Plotly y Streamlit.
+* **Replicabilidad:** Puedes encontrar la base de desarrollo y los datos en el GIT: .
 """)
